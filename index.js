e6aaf323756fdbd02b49fb76368fc5161a25f8da
ByronEsteban <91495851+ByronEsteban@users.noreply.github.com>
2022-11-22 09:17:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ import router from './Routes/router.js';
 
 const PORT = process.env.PORT || 4000;
 // const PORT = 3000;
-const DB = process.env.MONGODB_URI || 'mongodb://127.0.0.1/Preguntados';
-// const DB = 'mongodb+srv://preguntados:preguntados@preguntados.y0vjnwo.mongodb.net/preguntados?retryWrites=true&w=majority';
+// const DB = process.env.MONGODB_URI || 'mongodb://127.0.0.1/Preguntados';
+const DB = 'mongodb+srv://evagriodb:Jokerlal10@questions.q6gicwm.mongodb.net/?retryWrites=true&w=majority'
 
 const app = express();
 
